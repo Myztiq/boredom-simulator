cacaddd512fd4c643395a3c2cfc30e5e297ab16e
Myztiq <github@ryan-kahn.com>
2015-01-24 15:35:45 -0800

    Fixed iphone overflow issues.

--- assets/scripts/sms-writer.js
+++ assets/scripts/sms-writer.js
@@ -2,13 +2,14 @@ function SmsWriter() {
   this.recieveMessage = function (text) {
     var msgDIV = $("<div class='text-left'><p class='white-shadow'>"+ text +"</p></div>");
     $('.iphone .container').append(msgDIV);
-
     $('.iphone').animate({ scrollTop: $('.iphone .container').height() });
+
   };
 
   this.sendMessage = function (text) {
     var msgDIV = $("<div class='text-right'><p class='white-shadow'>"+ text +"</p></div>");
     $('.iphone .container').append(msgDIV);
+
     $('.iphone').animate({ scrollTop: $('.iphone .container').height() });
   };
 }
