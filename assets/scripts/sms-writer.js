3786349de0d0a51414036689c1e2e37c3918e868
Myztiq <github@ryan-kahn.com>
2015-01-24 14:21:38 -0800

    Added all the images! ALL OF THEM!

--- assets/scripts/sms-writer.js
+++ assets/scripts/sms-writer.js
@@ -3,11 +3,14 @@ function SmsWriter() {
     var msgText = $("<p class='white-shadow'>"+ text +"</p>");
     var msgDIV = $("<div class='text-left'><p class='white-shadow'>"+ text +"</p></div>");
     $('.iphone .container').append(msgDIV);
+
+    $('.iphone .container').scrollTop(1000000);
   };
 
   this.sendMessage = function (text) {
     var msgText = $("<p class='white-shadow'>"+ text +"</p>");
     var msgDIV = $("<div class='text-right'><p class='white-shadow'>"+ text +"</p></div>");
     $('.iphone .container').append(msgDIV);
+    $('.iphone .container').scrollTop(1000000);
   };
 }
