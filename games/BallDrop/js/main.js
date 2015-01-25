84fdc689c447ee1cb5a601dad851e339131bd4e0
Max Gordon <gordonmaxc@gmail.com>
2015-01-24 18:09:55 -0800

    kinda fixed ball drop

--- games/BallDrop/js/main.js
+++ games/BallDrop/js/main.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementsByTagName("canvas")[0];
 canvas.style.width = '100%';
 canvas.onclick = function (e) {
-  var circle = Bodies.circle(e.x*((484/800)+1), e.y*((363/600)+1), 30);
+  // var circle = Bodies.circle(e.x*((713/800)+1), e.y*((535/600)+1), 30);
+  var circle = Bodies.circle(e.x, e.y, 30);
   World.add(engine.world,[circle]);
 };
