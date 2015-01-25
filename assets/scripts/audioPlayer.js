1e13bdbb4987370e0bc168affde48743de91e9c7
Myztiq <github@ryan-kahn.com>
2015-01-25 12:02:42 -0800

    Moved audio around, cleaned up loading.

--- assets/scripts/audioPlayer.js
+++ assets/scripts/audioPlayer.js
@@ -1,30 +1,39 @@
 function AudioPlayer() {
 
+  var loadAudio = function(soundName, id){
+    lowLag.load(["/assets/sounds/"+soundName+".ogg","/assets/sounds/MP3/"+soundName+".mp3"],id);
+  }
+
   var loadSounds = function () {
     lowLag.init();
-    lowLag.load(["/assets/sounds/test_ogg.ogg"],"test");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_ui_txt_rcve_01.ogg", "/assets/sounds/MP3/bsim_sfx_ui_txt_rcve_01.mp3"],"txtReceive_01");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_ui_txt_rcve_02.ogg", "/assets/sounds/MP3/bsim_sfx_ui_txt_rcve_02.mp3"],"txtReceive_02");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_ui_txt_rcve_03.ogg", "/assets/sounds/MP3/bsim_sfx_ui_txt_rcve_03.mp3"],"txtReceive_03");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_ui_txt_snd_01.ogg", "/assets/sounds/MP3/bsim_sfx_ui_txt_snd_01.mp3"],"txtSend_01");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_ui_txt_snd_02.ogg", "/assets/sounds/MP3/bsim_sfx_ui_txt_snd_02.mp3"],"txtSend_02");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_ui_txt_snd_03.ogg", "/assets/sounds/MP3/bsim_sfx_ui_txt_snd_03.mp3"],"txtSend_03");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_lght_off.ogg", "/assets/sounds/MP3/bsim_sfx_lght_off.mp3"],"lightOff");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_lght_on.ogg","/assets/sounds/MP3/bsim_sfx_lght_on.mp3"],"lightOn");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_sigh_01.ogg","/assets/sounds/MP3/bsim_sfx_dx_sigh_01.mp3"],"bsim_sfx_dx_sigh_01");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_sigh_02.ogg","/assets/sounds/MP3/bsim_sfx_dx_sigh_02.mp3"],"bsim_sfx_dx_sigh_02");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_sigh_03.ogg","/assets/sounds/MP3/bsim_sfx_dx_sigh_03.mp3"],"bsim_sfx_dx_sigh_03");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_fart_03.ogg","/assets/sounds/MP3/bsim_sfx_fart_03.mp3"],"bsim_sfx_fart_03");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_crak_nukl.ogg","/assets/sounds/MP3/bsim_sfx_crak_nukl.mp3"],"bsim_sfx_crak_nukl");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_exer_02.ogg","/assets/sounds/MP3/bsim_sfx_dx_exer_02.mp3"],"bsim_sfx_dx_exer_02");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_exer_03.ogg","/assets/sounds/MP3/bsim_sfx_dx_exer_03.mp3"],"bsim_sfx_dx_exer_03");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_exer_04.ogg","/assets/sounds/MP3/bsim_sfx_dx_exer_04.mp3"],"bsim_sfx_dx_exer_04");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_exer_05.ogg","/assets/sounds/MP3/bsim_sfx_dx_exer_05.mp3"],"bsim_sfx_dx_exer_05");
-    // snoring from https://www.freesound.org/people/smidoid/sounds/63349/
-    lowLag.load(["/assets/sounds/OGG/snoring.ogg","/assets/sounds/MP3/snoring.mp3"],"snoring");
-    lowLag.load(["/assets/sounds/OGG/bsim_sfx_hovr.ogg","/assets/sounds/MP3/bsim_sfx_hovr.mp3"],"hovr");
-    lowLag.load(["/assets/sounds/OGG/bsm_sfx_ui_gam_slct.ogg","/assets/sounds/MP3/bsm_sfx_ui_gam_slct.mp3"],"gameSelect");
+    loadAudio('bsim_sfx_ui_txt_rcve_01', 'txtReceive_01');
+    loadAudio('bsim_sfx_ui_txt_rcve_02', 'txtReceive_02');
+    loadAudio('bsim_sfx_ui_txt_rcve_03', 'txtReceive_03');
 
+    loadAudio('bsim_sfx_ui_txt_snd_01', 'txtSend_01');
+    loadAudio('bsim_sfx_ui_txt_snd_02', 'txtSend_02');
+    loadAudio('bsim_sfx_ui_txt_snd_03', 'txtSend_03');
+
+    loadAudio('bsim_sfx_lght_off', 'lightOff');
+    loadAudio('bsim_sfx_lght_on', 'lightOn');
+
+
+    loadAudio('bsim_sfx_dx_sigh_01', 'bsim_sfx_dx_sigh_01');
+    loadAudio('bsim_sfx_dx_sigh_02', 'bsim_sfx_dx_sigh_02');
+    loadAudio('bsim_sfx_dx_sigh_03', 'bsim_sfx_dx_sigh_03');
+    loadAudio('bsim_sfx_lght_on', 'lightOn');
+
+    loadAudio('bsim_sfx_fart_03', 'bsim_sfx_fart_03');
+    loadAudio('bsim_sfx_crak_nukl', 'bsim_sfx_crak_nukl');
+    loadAudio('bsim_sfx_dx_exer_02', 'bsim_sfx_dx_exer_02');
+    loadAudio('bsim_sfx_dx_exer_03', 'bsim_sfx_dx_exer_03');
+    loadAudio('bsim_sfx_dx_exer_04', 'bsim_sfx_dx_exer_04');
+    loadAudio('bsim_sfx_dx_exer_05', 'bsim_sfx_dx_exer_05');
+
+    // snoring from https://www.freesound.org/people/smidoid/sounds/63349/
+    loadAudio('snoring', 'snoring');
+    loadAudio('bsim_sfx_hovr', 'hovr');
+    loadAudio('bsm_sfx_ui_gam_slct', 'gameSelect');
   };
 
   var play = function (name) {
