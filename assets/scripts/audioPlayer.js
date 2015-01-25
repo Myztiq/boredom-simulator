3393e7826ec7182d938ff0e362211b66a3db3502
Max Gordon <gordonmaxc@gmail.com>
2015-01-25 11:44:18 -0800

    mf

--- assets/scripts/audioPlayer.js
+++ assets/scripts/audioPlayer.js
@@ -11,7 +11,6 @@ function AudioPlayer() {
     lowLag.load(["/assets/sounds/OGG/bsim_sfx_ui_txt_snd_03.ogg", "/assets/sounds/MP3/bsim_sfx_ui_txt_snd_03.mp3"],"txtSend_03");
     lowLag.load(["/assets/sounds/OGG/bsim_sfx_lght_off.ogg", "/assets/sounds/MP3/bsim_sfx_lght_off.mp3"],"lightOff");
     lowLag.load(["/assets/sounds/OGG/bsim_sfx_lght_on.ogg","/assets/sounds/MP3/bsim_sfx_lght_on.mp3"],"lightOn");
-<<<<<<< HEAD
     lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_sigh_01.ogg","/assets/sounds/MP3/bsim_sfx_dx_sigh_01.mp3"],"bsim_sfx_dx_sigh_01");
     lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_sigh_02.ogg","/assets/sounds/MP3/bsim_sfx_dx_sigh_02.mp3"],"bsim_sfx_dx_sigh_02");
     lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_sigh_03.ogg","/assets/sounds/MP3/bsim_sfx_dx_sigh_03.mp3"],"bsim_sfx_dx_sigh_03");
@@ -21,11 +20,9 @@ function AudioPlayer() {
     lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_exer_03.ogg","/assets/sounds/MP3/bsim_sfx_dx_exer_03.mp3"],"bsim_sfx_dx_exer_03");
     lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_exer_04.ogg","/assets/sounds/MP3/bsim_sfx_dx_exer_04.mp3"],"bsim_sfx_dx_exer_04");
     lowLag.load(["/assets/sounds/OGG/bsim_sfx_dx_exer_05.ogg","/assets/sounds/MP3/bsim_sfx_dx_exer_05.mp3"],"bsim_sfx_dx_exer_05");
-=======
     // snoring from https://www.freesound.org/people/smidoid/sounds/63349/
     lowLag.load(["/assets/sounds/OGG/snoring.ogg","/assets/sounds/MP3/snoring.mp3"],"snoring");
 
->>>>>>> 2182f43d4a7876cdd6b9950b5d6dba6da01e1f17
   };
 
   var play = function (name) {
